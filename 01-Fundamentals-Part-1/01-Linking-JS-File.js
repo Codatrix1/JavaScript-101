82b55a6dfdcdf6e447ecba45b17fc9ec7b945ac4
Ankur Sinha <ankur.codatrix@gmail.com>
2024-09-04 21:06:03 +0530

    Linking JS File

---
+++ 01-Fundamentals-Part-1/01-Linking-JS-File.js
@@ -0,0 +1,4 @@
+let js = "amazing";
+if (js === "amazing") console.log("JavaScript is FUN!");
+
+console.log(12 + 24 + 10);
--- 01-Fundamentals-Part-1/script.js
+++
@@ -1,4 +0,0 @@
-let js = "amazing";
-if (js === "amazing") console.log("JavaScript is FUN!");
-
-console.log(34 + 24 + 10);
